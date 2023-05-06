621f14212b032c2612c0005e0b775512b23348bb
Yuri Sampaio <ynogueira317@gmail.com>
2023-05-05 21:36:21 -0300

    adicionando o popup quando o botão registrar é clicado

--- js/cadastroDoPaciente.js
+++ js/cadastroDoPaciente.js
@@ -1,12 +1,35 @@
+// abre a sideNav 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 
     document.body.style.backgroundColor = B9DDE8;
 }
 
+// Fecha a sideNav
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = B9DDE8;
 }
 
-
+document.getElementById("meuFormulario").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+  
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+  
+    if (nome !== "" && email !== "") {
+      exibirPopup();
+    } else {
+      alert("Por favor, preencha todos os campos do formulário.");
+    }
+  });
+  
+  function exibirPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+  }
+  
+  function fecharPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+  }
