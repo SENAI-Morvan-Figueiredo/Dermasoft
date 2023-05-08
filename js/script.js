5f471cbe55ecb9abc2e1ad4703b3ca7b2c5e4804
Yuri Sampaio <sampaio.yuri31@gmail.com>
2023-05-07 22:46:22 -0300

    transição para os textos sobrepostos do banner emergirem

--- js/script.js
+++ js/script.js
@@ -1,29 +1,38 @@
-window.addEventListener('load', function() {
-    var textoElement = document.querySelector('.textoSobreposto1');
-  
-    setTimeout(function() {
-      textoElement.style.opacity = '1';
-      textoElement.style.transform = 'translateY(0)';
+
+
+// faz os textos 1 e 2 do banner emergirem
+window.addEventListener('load', function () {
+    var textoElement1 = document.querySelector('.textoSobreposto1');
+    var textoElement2 = document.querySelector('.textoSobreposto2');
+
+    setTimeout(function () {
+        textoElement1.style.opacity = '1';
+        textoElement1.style.transform = 'translateY(0)';
     }, 800); // Tempo de atraso em milissegundos antes do surgimento do texto
-  });
   
   
-          // faz os icones de coração e pasta surgirem 
-          functionadicionaClasseAoRolar(); {
-              // seleciona a div
-  
-              const minhaDiv = document.querySelector('.doutor');
-  
-              // determina o ponto de rolagem da tela
-              const PontoDeRolagem = 700;
-  
-              var alturaTela = window.innerHeight;
-              var posicaoScroll = window.scrollY || window.pageYOffset;
-  
-              window.addEventListener('scroll', function () {
-                  if (posicaoScroll > alturaTela) {
-                      minhaDiv.classList.add('.doutorAtivo');
-                  }
-              });
-          }
-  
+    setTimeout(function () {
+        textoElement2.style.opacity = '1';
+        textoElement2.style.transform = 'translateY(0)';
+    }, 1000); // Tempo de atraso em milissegundos antes do surgimento do texto
+});
+
+
+// faz os icones de coração e pasta surgirem 
+functionadicionaClasseAoRolar(); {
+    // seleciona a div
+
+    const minhaDiv = document.querySelector('.doutor');
+
+    // determina o ponto de rolagem da tela
+    const PontoDeRolagem = 700;
+
+    var alturaTela = window.innerHeight;
+    var posicaoScroll = window.scrollY || window.pageYOffset;
+
+    window.addEventListener('scroll', function () {
+        if (posicaoScroll > alturaTela) {
+            minhaDiv.classList.add('.doutorAtivo');
+        }
+    });
+}
