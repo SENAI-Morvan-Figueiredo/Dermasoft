6864cda143d7164c8d54e03d7cde7811ae74126a
Yuri Sampaio <ynogueira317@gmail.com>
2023-05-08 21:43:12 -0300

    alterando o ponto de rolagem

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function adicionaClasseAoRolar() {
     const minhaDiv = document.querySelector('.animacao');
   
     // determina o ponto de rolagem da tela
-    const pontoDeRolagem = 100;
+    const pontoDeRolagem = 50;
   
     // adiciona a classe "ativo" quando a página é rolada para o ponto
     window.addEventListener('scroll', function () {
