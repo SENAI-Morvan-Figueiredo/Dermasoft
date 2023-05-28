de9ed226364efab205b8eeec8df10574efaf7353
Yuri Sampaio <sampaio.yuri31@gmail.com>
2023-05-27 23:33:48 -0300

    atualizando as paginas cadastro e login

---
+++ js/login.js
@@ -0,0 +1,12 @@
+// abre a sideNav 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+
+    document.body.style.backgroundColor = B9DDE8;
+}
+
+// Fecha a sideNav
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = B9DDE8;
+}
