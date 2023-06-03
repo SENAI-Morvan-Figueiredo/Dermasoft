5f4b9c57bf2ca50ee1f9fe52e282854dcc88307a
Yuri Nogueira <ynogueira317@gmail.com>
2023-06-02 21:33:55 -0300

    atualizando as paginas estetica corporal e conheca a dermasift para adicionar a sidebar ao diminuir a tela

--- js/esteticaCorporal.js
+++ js/esteticaCorporal.js
@@ -19,3 +19,16 @@ window.addEventListener('load', function () {
   
   });
   
+
+  // abre a sideNav 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+
+  document.body.style.backgroundColor = B9DDE8;
+}
+
+// Fecha a sideNav
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = B9DDE8;
+}
