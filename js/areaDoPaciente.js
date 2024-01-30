184c9aa2a679e18cf623098305c74c06316fcc80
Yuri Nogueira <ynogueira317@gmail.com>
2024-01-29 22:01:03 -0300

    ajustando a tela do paciente logado

--- js/areaDoPaciente.js
+++ js/areaDoPaciente.js
@@ -12,3 +12,21 @@ function closeNav() {
 }
 
 
+
+function toggleDropdown(dropdown) {
+    // Adiciona ou remove a classe "active" para mostrar ou ocultar o conteúdo do menu suspenso
+    dropdown.classList.toggle('active');
+
+    // Fecha os outros menus suspensos
+    var dropdowns = document.querySelectorAll('.dropdown');
+    dropdowns.forEach(function (otherDropdown) {
+      if (otherDropdown !== dropdown && otherDropdown.classList.contains('active')) {
+        otherDropdown.classList.remove('active');
+      }
+    });
+  }
+
+  // Adiciona ou remove a classe "active" para mostrar ou ocultar o conteúdo da sidebar
+  function toggleSidebar(sidebar) {
+    sidebar.classList.toggle('active');
+  }
