57f3cab4eb52950799b585555527704457443ee0
Yuri Sampaio <ynogueira317@gmail.com>
2024-04-16 22:28:39 -0300

    isolando as configurações do banco de dados do settyngs.py

--- DermaSoft/settings.py
+++ DermaSoft/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = "DermaSoft.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": config(
+        "DATABASE_URL", default=f'sqlite:///{BASE_DIR /   "db.sqlite3"}', 
+        cast=db_url,
+        )
 }
 
 
