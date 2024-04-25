3b13e37d101ff37ea1312d3d1a2bd07ef6340022
Yuri Sampaio <ynogueira317@gmail.com>
2024-04-24 21:51:40 -0300

    colocando os apps no settings.py

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "usuarios.apps.UsuariosConfig",
+    "medico.apps.MedicoConfig",
     "home.apps.HomeConfig",
     "paciente.apps.PacienteConfig"
     "usuarios.apps.UsuariosConfig"
