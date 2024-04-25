b7a41f3ad47fda3849b12230ca9885aa14945db7
jorgevendramini <jorgemunhoz_88@hotmail.com>
2024-04-24 21:16:26 -0300

    add urls para back end e static files path

--- core/settings.py
+++ core/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "templates/static"),)
+STATIC_ROOT = os.path.join("static")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
