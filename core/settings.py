8c81fa66182749b99d3a4b8b81c3e506c928e53e
jorgevendramini <jorgemunhoz_88@hotmail.com>
2024-05-09 21:55:07 -0300

    feat: struggling to make pw reset work

--- core/settings.py
+++ core/settings.py
@@ -146,3 +146,12 @@ MESSAGE_TAGS = {
     constants.INFO: "alert-info",
     constants.WARNING: "alert-warning",
 }
+
+# Esqueci a senha email config
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,9 +1,37 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("cadastro/", views.cadastro, name="cadastro"),
     path("login/", views.login_view, name="login"),
     path("sair/", views.sair, name="sair"),
-    path("esqueci/", views.esqueci, name="esqueci"),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="password/password-reset.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="password/password-reset-done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="password/password-reset-confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="password/password-reset-complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
