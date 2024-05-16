a11bce98784516fdd3634d6298a92f5fa7899fa0
jorgevendramini <jorgemunhoz_88@hotmail.com>
2024-05-15 22:02:59 -0300

    still fixing forgot pw

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
 from django.contrib import auth
-from django.contrib.auth import views as auth_views
 
 
 def cadastro(request):
