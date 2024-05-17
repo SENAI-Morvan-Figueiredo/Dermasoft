129c5f7bd530eb4f29196baa44b1aa8c148b72e4
jorgevendramini <jorgemunhoz_88@hotmail.com>
2024-05-16 21:12:52 -0300

    sqlite add para apresentação

--- core/settings.py
+++ core/settings.py
@@ -153,7 +153,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+#EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+#EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
 # AUTH_USER_MODEL = "accounts.User"
